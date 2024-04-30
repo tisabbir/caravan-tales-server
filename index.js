aaa6ef696a1bfd191c921e6b6f672e85e46eca2a
TI Sabbir <tisabbir0@gmail.com>
2024-04-30 18:13:01 +0600

    all tourist spot page section done Alhamdlulillah

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-const express = require('express')
-const cors = require('cors');
-require('dotenv').config();
-const app = express()
-
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
 
 //middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
+const port = process.env.PORT || 5000;
 
-
-
-const port = process.env.PORT || 5000
-
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://tisabbir0:UttAJAHVh8q3884f@cluster0.ealfozn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri =
+  "mongodb+srv://tisabbir0:UttAJAHVh8q3884f@cluster0.ealfozn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -33,18 +27,45 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const spots = client.db('spotsDB').collection("spots");
+    const spots = client.db("spotsDB").collection("spots");
+    const allSpots = client.db("spotsDB").collection("allSpots");
 
-    app.get('/spots', async(req,res)=>{
+    app.get("/spots", async (req, res) => {
       const cursor = spots.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/spots/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await spots.findOne(query);
+      res.send(result);
+    });
+
+
+    app.get('/allspots', async(req,res)=>{
+      const cursor = allSpots.find();
       const result =  await cursor.toArray();
       res.send(result)
-  }) 
-    
+  })
+  app.get("/allspots/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await allSpots.findOne(query);
+    res.send(result);
+  });
+    app.post("/allspots", async (req, res) => {
+      const newSpot = req.body;
+      console.log(newSpot);
+      const result = await allSpots.insertOne(newSpot);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -52,13 +73,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-  res.send('Thousands of caravan tales created with beautiful travels!')
-})
+app.get("/", (req, res) => {
+  res.send("Thousands of caravan tales created with beautiful travels!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
