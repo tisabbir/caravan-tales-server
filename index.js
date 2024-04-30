8b422b967b01c97b56c3d08210134158f3167b49
TI Sabbir <tisabbir0@gmail.com>
2024-04-30 23:51:53 +0600

    tried to created update

--- index.js
+++ index.js
@@ -73,6 +73,8 @@ async function run() {
       res.send(result);
     });
 
+    // app.put("")
+
     app.delete("/allspots/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
