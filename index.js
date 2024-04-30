d5cb15a8e575905d71bbebc4415fea274a87e777
TI Sabbir <tisabbir0@gmail.com>
2024-04-30 22:57:51 +0600

    tried to solve deploy related problem

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri =
-  "mongodb+srv://tisabbir0:UttAJAHVh8q3884f@cluster0.ealfozn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ealfozn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const spots = client.db("spotsDB").collection("spots");
     const allSpots = client.db("spotsDB").collection("allSpots");
@@ -81,7 +80,7 @@ async function run() {
       res.send(result)
     })
 
-    // Country 
+    // Country    
     app.get("/country", async (req, res) => {
       const cursor = countryCollection.find();
       const result = await cursor.toArray();
